dd160c13d6acfbe556c43c956b67b27e0c843313
Sumit Santosh Sahu <sssahu1009@gmail.com>
2022-05-07 18:19:50 +0530

    Final Changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { Fragment } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, {useState} from 'react';
 import Alert from './components/Alert';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -39,20 +39,20 @@ function App() {
   }
   return (
     <>
-    {/* <Router> */}
-    <Navbar title="MY-APPS" mode={mode} toggleMode={toggleMode}/>
+    <Router>
+    <Navbar title="Alpha Utils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3">
-    {/* <Switch>
+    <Switch>
           <Route exact path="/about">
-            <About />
+            <About mode={mode} />
           </Route>
-          <Route exact path="/"> */}
-            <TextForm showAlert={showAlert} heading="Enter the text to analyze mode" mode={mode}/>
-          {/* </Route>
-        </Switch> */}
+          <Route exact path="/">
+            <TextForm showAlert={showAlert} heading="Alpha Utils : WordsCounter and CharactersCounter" mode={mode}/>
+          </Route>
+        </Switch>
     </div>
-    {/* </Router> */}
+    </Router>
     </>
   );
 }
